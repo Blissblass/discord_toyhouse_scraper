f2fde1485a988c2ba5dc98612d5a4fbdc377ce06
Lily <lolpo12345@outlook.com>
2021-11-24 01:24:13 +0300

    Complete fetch request using puppeteer

--- commands/scrapeTest.js
+++ commands/scrapeTest.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const request = require('request-promise');
-const cheerio = require('cheerio');
+const puppeteer = require("puppeteer");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,23 +8,25 @@ module.exports = {
     .addStringOption(option => option.setName('link').setDescription('Enter a Toyhouse link for scraping.')),
 
 	async execute(interaction) {
-    const link = interaction.options.getString('link').replace(/\s/g, "");
+    const link = interaction.options.getString('link');
     if(!link || !link.startsWith('https://toyhou.se/')) {
+      link = link.replace(/\s/g, "");
       await interaction.reply({content: `<@${interaction.user.id}>, please give me a Toyhouse link!`, ephemeral: true});
       return
     }
 
-    request(link, async (error, response, html) => {
-      if(!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-        const name = $("h1.display-4").text();
-        await interaction.reply(`<@${interaction.user.id}>, you passed in: ${link} and successfully returned a response for ${name}!`);
-        console.log('all operations successful!');
-        return
-      } else {
-        await interaction.reply(error);
-        return
-      }
-    });
+    const fetchSite = async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto(link);
+
+      const nameEle = await page.waitForSelector("h1.display-4");
+      const name = await nameEle.evaluate(el => el.textContent);
+
+      await interaction.reply(`<@${interaction.user.id}>, fetched profile for ${name}`);
+      await browser.close();
+    };
+
+    fetchSite();
 	},
 };
