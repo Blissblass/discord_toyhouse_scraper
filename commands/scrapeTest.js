e2bf97179553643edd8d3ef7144d509d03212291
Lily <lolpo12345@outlook.com>
2021-11-24 00:55:19 +0300

    Add fetching username for toyhouse profile

--- commands/scrapeTest.js
+++ commands/scrapeTest.js
@@ -9,7 +9,7 @@ module.exports = {
     .addStringOption(option => option.setName('link').setDescription('Enter a Toyhouse link for scraping.')),
 
 	async execute(interaction) {
-    const link = interaction.options.getString('link');
+    const link = interaction.options.getString('link').replace(/\s/g, "");
     if(!link || !link.startsWith('https://toyhou.se/')) {
       await interaction.reply({content: `<@${interaction.user.id}>, please give me a Toyhouse link!`, ephemeral: true});
       return
@@ -18,8 +18,9 @@ module.exports = {
     request(link, async (error, response, html) => {
       if(!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
+        const name = $("h1.display-4").text();
+        await interaction.reply(`<@${interaction.user.id}>, you passed in: ${link} and successfully returned a response for ${name}!`);
         console.log('all operations successful!');
-        await interaction.reply(`<@${interaction.user.id}>, you passed in: ${link} and successfully returned a response!`);
         return
       } else {
         await interaction.reply(error);
