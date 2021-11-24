f0dfc33d48ffaae5f1ee9f3280432feae9553115
Lily <lolpo12345@outlook.com>
2021-11-24 04:15:15 +0300

    Fixes #1: Client now uses puppeteer to click on popups and thus gains access to necessary data

--- commands/scrapeTest.js
+++ commands/scrapeTest.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const puppeteer = require("puppeteer");
+const fetch = require('node-fetch');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,9 +11,10 @@ module.exports = {
 	async execute(interaction) {
     let link = interaction.options.getString('link');
     if(!link || !link.startsWith('https://toyhou.se/')) {
-      await interaction.reply({content: `<@${interaction.user.id}>, please give me a Toyhouse link!`, ephemeral: true});
+      await interaction.reply({content: `<@${interaction.user.id}>, please give me a Toyhouse link!`, ephemeral: true})
       return
     }
+
     link = link.replace(/\s/g, "");
 
     const fetchSite = async () => {
@@ -20,11 +22,28 @@ module.exports = {
       const page = await browser.newPage();
       await page.goto(link);
 
-      const nameEle = await page.waitForSelector("h1.display-4");
-      const name = await nameEle.evaluate(el => el.textContent);
+      const nameEle = await page.$("h1.display-4");
+      
+      if(nameEle) {
+        const name = await nameEle.evaluate(el => el.textContent);
 
-      await interaction.reply(`<@${interaction.user.id}>, fetched profile for ${name}`);
-      await browser.close();
+        await interaction.reply(`<@${interaction.user.id}>, fetched profile for ${name}`);
+        await browser.close();
+        return
+      } else {
+        await interaction.deferReply();
+        const confirmButton = await page.$("input.btn-success");
+        await confirmButton.click();
+        await page.waitForSelector('h1.display-4', { visible: true });
+        page.screenshot({ path: 'scrunkly.png' });  
+        
+        const nameEle = await page.$("h1.display-4");
+        const name = await nameEle.evaluate(el => el.textContent);
+
+        await interaction.editReply(`<@!${interaction.user.id}>, fetched profile for ${name}`);
+        await browser.close();
+        return
+      }
     };
 
     fetchSite();
